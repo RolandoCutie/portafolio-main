ca766f02b208863ce21081a00e5889751d3c15db
Rolando Garcia Cutie <rolando.garcia@alivi.com>
2023-10-16 22:40:30 -0400

    add some changues in the profile

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
- function alert(){
-   message => {
-    if(message == 'OK'){
-        swal("Good job!", "You clicked the button!", "success");
+function alert() {
+  (message) => {
+    if (message == "OK") {
+      swal("Good job!", "You clicked the button!", "success");
+    } else {
+      swal("Ups, this went bad!", "You clicked the button!", "error");
     }
-    else{
-        swal("Ups, this went bad!", "You clicked the button!", "error");
-    }
-   }
- }
+  };
+}
 
- function openPopup() {
-    document.getElementById('popup').style.display = 'block';
-  }
-  
-  function closePopup() {
-    document.getElementById('popup').style.display = 'none';
-  }
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function openPopupAtaja() {
+  document.getElementById("popupAtaja").style.display = "block";
+}
+
+function openPopupAlivi() {
+  document.getElementById("popupAlivi").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function closePopupAtaja() {
+  document.getElementById("popupAtaja").style.display = "none";
+}
+
+function closePopupAlivi() {
+  document.getElementById("popupAlivi").style.display = "none";
+}
